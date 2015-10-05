9e7334b3440a1cbd779c0c707b11415dff55b497
K M Masum Habib <masum.habib@gmail.com>
2015-10-04 23:04:05 -0700

    Add GPL license

--- todo_shell.sh
+++ todo_shell.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+#   A simple "shell" for Gina Trapani's todo.txt-cli
+#    Copyright (C) 2015 K M Masum Habib <masumhabib.com>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
 
 # default parameters
 # -----------------------------------------------------------------------------
